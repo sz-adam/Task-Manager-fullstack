ede9fc722fe7a02954a3c53b82c64eb2a8fcf26c
Szabó Ádám <szaboadam21@gmail.com>
2024-10-08 21:02:05 +0200

    create taskmodel

---
+++ mobile/lib/model/task_model.dart
@@ -0,0 +1,38 @@
+class TaskModel {
+  int id;
+  String title;
+  String description;
+  String status;
+  DateTime createdAt;
+ 
+  TaskModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.status,
+    required this.createdAt,
+  });
+
+  // JSON-ból TaskModel objektumot készít
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      status: json['status'],
+      createdAt: DateTime.parse(json['created_at']),
+    );
+  }
+
+  // TaskModel objektumból JSON-t készít
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'status': status,
+      'created_at': createdAt
+         
+    };
+  }
+}
