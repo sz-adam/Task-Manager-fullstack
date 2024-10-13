48381dd55797e29cc8f1eca8f60fa4929e439d37
Szabó Ádám <szaboadam21@gmail.com>
2024-10-13 20:01:22 +0200

    animated search

--- mobile/lib/widget/header.dart
+++ mobile/lib/widget/header.dart
@@ -1,3 +1,4 @@
+import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/widget/create_update_task.dart';
 
@@ -9,24 +10,54 @@ class Header extends StatefulWidget {
 }
 
 class _HeaderState extends State<Header> {
+  TextEditingController textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text("Task Manager", style: Theme.of(context).textTheme.titleLarge),
-          IconButton(
-            icon:const Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => CreateUpdatetask(),
+      child: Stack(
+        children: [      
+          Positioned(
+            left: 10,
+            top: 30,
+            child: Text("Task Manager",
+                style: Theme.of(context).textTheme.titleLarge),
+          ),
+          // Keresősáv és a + gomb
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: AnimSearchBar(
+                  width: 400,
+                  textController: textController,
+                  onSubmitted: (value) { },
+                  onSuffixTap: () {
+                    setState(() {
+                      textController.clear(); 
+                    });
+                  },
+                  color: Colors.white,
+                  helpText: "Search Text..." ,
+                  autoFocus: true,
+                  closeSearchOnSuffixTap: true,
+                  animationDurationInMilli: 1000,
+                  rtl: true,
                 ),
-              );
-            },
-          )
+              ),
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => CreateUpdatetask(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
